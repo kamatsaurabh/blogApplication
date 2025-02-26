0c0587654b3c2312297c75067b8431df516fb2ae
kamatsaurabh <saurabh.kamat@intelegencia.com>
2025-02-26 20:42:18 +0530

    \"Initial Client Pre-Requisite \"

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  Url:'http://localhost:3000',
+  GoogleAuthKey:'',
+  FacebookAuthKey:''
+};
---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  Url:'http://localhost:3000',
+  GoogleAuthKey:'',
+  FacebookAuthKey:''
+};
